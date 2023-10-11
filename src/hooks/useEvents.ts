f8333e51ac87c0749d7663a21ff33f9f12c56167
unknown <rrrr0ffff@gmail.com>
2023-10-11 18:05:22 -0300

    created useEvents hook

---
+++ src/hooks/useEvents.ts
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+import { Context } from "../context/Events";
+
+export const EVENT_COLORS = ["red", "green", "blue"] as const;
+
+export function useEvents() {
+	const value = useContext(Context);
+	if (value === null) {
+		throw new Error("useEvents must be used within an EventsProvider");
+	}
+	return value;
+}
