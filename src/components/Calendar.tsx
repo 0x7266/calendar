1401edf6fd5a1c836a89dbd47615fe8475968010
unknown <rrrr0ffff@gmail.com>
2023-10-11 17:38:06 -0300

    header buttons working

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -9,6 +9,8 @@ import {
 	endOfDay,
 	isBefore,
 	isToday,
+	subMonths,
+	addMonths,
 } from "date-fns";
 import { formatDate } from "../utils/formatDate";
 import { cc } from "../utils/cc";
@@ -24,12 +26,30 @@ export default function Calendar() {
 	return (
 		<div className="calendar">
 			<div className="header">
-				<button className="btn">Today</button>
+				<button className="btn" onClick={() => setSelectedMonth(new Date())}>
+					Today
+				</button>
 				<div>
-					<button className="month-change-btn">&lt;</button>
-					<button className="month-change-btn">&gt;</button>
+					<button
+						className="month-change-btn"
+						onClick={() => {
+							setSelectedMonth((m) => subMonths(m, 1));
+						}}
+					>
+						&lt;
+					</button>
+					<button
+						className="month-change-btn"
+						onClick={() => {
+							setSelectedMonth((m) => addMonths(m, 1));
+						}}
+					>
+						&gt;
+					</button>
 				</div>
-				<span className="month-title">June 2023</span>
+				<span className="month-title">
+					{formatDate(selectedMonth, { month: "long", year: "numeric" })}
+				</span>
 			</div>
 			<div className="days">
 				{calendarDays.map((day, index) => (
