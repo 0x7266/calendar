5361c312c1c1d3f960feba897ee7d55248a6517b
unknown <rrrr0ffff@gmail.com>
2023-10-16 00:05:17 -0300

    color buttons

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -14,7 +14,7 @@ import {
 } from "date-fns";
 import { formatDate } from "../utils/formatDate";
 import { cc } from "../utils/cc";
-import { useEvents } from "../hooks/useEvents";
+import { EVENT_COLORS, useEvents } from "../hooks/useEvents";
 import { Modal, ModalProps } from "./Modal";
 import { UnionOmit } from "../utils/types";
 import { Event } from "../context/Events";
@@ -144,6 +144,9 @@ function EventFormModal({
 }: EventFormModalProps) {
 	const isNew = event === null;
 	const formId = useId();
+	const [selectedColor, setSelectedColor] = useState(
+		event?.color || EVENT_COLORS[0]
+	);
 
 	return (
 		<Modal {...modalProps}>
@@ -157,65 +160,52 @@ function EventFormModal({
 			<form>
 				<div className="form-group">
 					<label htmlFor={`${formId}-name`}>Name</label>
-					<input type="text" name="name" id="name" />
+					<input type="text" name="name" id={`${formId}-name`} />
 				</div>
 				<div className="form-group checkbox">
-					<input type="checkbox" name="all-day" id="all-day" />
-					<label htmlFor="all-day">All Day?</label>
+					<input type="checkbox" name="all-day" id={`${formId}-all-day`} />
+					<label htmlFor={`${formId}-all-day`}>All Day?</label>
 				</div>
 				<div className="row">
 					<div className="form-group">
-						<label htmlFor="start-time">Start Time</label>
-						<input type="time" name="start-time" id="start-time" />
+						<label htmlFor={`${formId}-start-time`}>Start Time</label>
+						<input type="time" name="start-time" id={`${formId}-start-time`} />
 					</div>
 					<div className="form-group">
-						<label htmlFor="end-time">End Time</label>
-						<input type="time" name="end-time" id="end-time" />
+						<label htmlFor={`${formId}-end-time`}>End Time</label>
+						<input type="time" name="end-time" id={`${formId}-end-time`} />
 					</div>
 				</div>
 				<div className="form-group">
 					<label>Color</label>
 					<div className="row left">
-						<input
-							type="radio"
-							name="color"
-							value="blue"
-							id="blue"
-							checked
-							className="color-radio"
-						/>
-						<label htmlFor="blue">
-							<span className="sr-only">Blue</span>
-						</label>
-						<input
-							type="radio"
-							name="color"
-							value="red"
-							id="red"
-							className="color-radio"
-						/>
-						<label htmlFor="red">
-							<span className="sr-only">Red</span>
-						</label>
-						<input
-							type="radio"
-							name="color"
-							value="green"
-							id="green"
-							className="color-radio"
-						/>
-						<label htmlFor="green">
-							<span className="sr-only">Green</span>
-						</label>
+						{EVENT_COLORS.map((color) => (
+							<>
+								<input
+									type="radio"
+									name="color"
+									value={color}
+									id={`${formId}-${color}`}
+									checked={selectedColor === color}
+									onChange={() => setSelectedColor(color)}
+									className="color-radio"
+								/>
+								<label htmlFor={`${formId}-${color}`}>
+									<span className="sr-only">{color}</span>
+								</label>
+							</>
+						))}
 					</div>
 				</div>
 				<div className="row">
 					<button className="btn btn-success" type="submit">
-						Add
+						{isNew ? "Add" : "Edit"}
 					</button>
-					<button className="btn btn-delete" type="button">
-						Delete
-					</button>
+					{onDelete !== null && (
+						<button className="btn btn-delete" type="button" onClick={onDelete}>
+							Delete
+						</button>
+					)}
 				</div>
 			</form>
 		</Modal>
