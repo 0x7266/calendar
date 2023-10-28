7090211634d7c563ff51b82a774ded6169df77a9
unknown <rrrr0ffff@gmail.com>
2023-10-28 00:03:01 -0300

    CalendarEvent

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -12,6 +12,7 @@ import {
 	subMonths,
 	addMonths,
 	isSameDay,
+	parse,
 } from "date-fns";
 import { formatDate } from "../utils/formatDate";
 import { cc } from "../utils/cc";
@@ -153,6 +154,28 @@ function CalendarDay({
 				onSubmit={addEvent}
 			/>
 		</div>
+	);
+}
+
+function CalendarEvent({ event }: { event: Event }) {
+	return (
+		<button
+			className={cc("event", event.color, event.allDay && "all-day-event")}
+		>
+			{event.allDay ? (
+				<div className="event-name">{event.name}</div>
+			) : (
+				<>
+					<div className={`color-dot ${event.color}`}></div>
+					<div className="event-time">
+						{formatDate(parse(event.startTime, "HH:mm", event.date), {
+							timeStyle: "short",
+						})}
+					</div>
+					<div className="event-name">{event.name}</div>
+				</>
+			)}
+		</button>
 	);
 }
 
